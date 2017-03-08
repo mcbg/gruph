84824da521b51cbba11693feb13562e331bd1035
Michael Galanakis <mgalanakis@sund.ku.dk>
2017-03-08 02:29:49 +0100

    changed style for braces

--- rcpp/fixed_length_priority_queue.cpp
+++ rcpp/fixed_length_priority_queue.cpp
@@ -4,18 +4,24 @@
 // [[Rcpp::plugins(cpp11)]]
 
 template <typename T, class F>
-class fl_priority_queue {
+class fl_priority_queue
+{
   std::priority_queue< T, std::vector<T>, F> queue;
   F cmp = F();
+  
 public:
-  fl_priority_queue(int n, T x) {
-    for(int i = 0; i < n; ++i) {
+  fl_priority_queue(int n, T x)
+  {
+    for(int i = 0; i < n; ++i)
+    {
       queue.push(x);
     }
   }
   
-  bool attempt_push(T x) {
-    if (cmp(x, queue.top())) {
+  bool attempt_push(T x)
+  {
+    if (cmp(x, queue.top()))
+    {
       queue.pop();
       queue.push(x);
       return true;
@@ -23,12 +29,13 @@ public:
     return false;
   }
   
-  T top() {
+  T top()
+  {
     return queue.top();
   }
   
-  void copy_to_vector(T *v) {
+  void copy_to_vector(T *v)
+  {
     std::copy(&(queue.top()), &(queue.top()) + queue.size(), v);
   }
-  
 };
