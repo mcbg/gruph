ff5552d06c025e825307e45ad03d4b78bd362878
Michael Galanakis <mgalanakis@sund.ku.dk>
2017-03-08 02:30:34 +0100

    made class to calculate mutual information

---
+++ rcpp/gaussian_mutual_information.cpp
@@ -0,0 +1,70 @@
+#include <Rcpp.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace Rcpp;
+
+// [[Rcpp::plugins(cpp14)]]
+// [[Rcpp::plugins(cpp11)]]
+
+typedef std::vector<double> vecdub;
+typedef std::pair<vecdub::iterator, vecdub::iterator> pairit;
+typedef std::pair<int, int> edge;
+typedef std::pair<double, edge> w_edge;
+
+
+class gaussian_mutual_information
+{
+  double mean(vecdub x)
+  {
+    double ourmean = 0;
+    
+    for(auto num : x) 
+      ourmean += num / x.size();
+    
+    return ourmean;
+  }
+  
+  void normalize(vecdub &x)
+  { 
+    double m  = mean(x);
+    
+    for(auto &num : x)
+      num -= m;
+  }
+  
+  double sd(vecdub x)
+  {
+    double mysd = 0;
+    normalize(x);
+    
+    for(double num : x)
+      mysd += std::pow(num, 2);
+    
+    mysd /= x.size() - 1;
+    return std::sqrt(mysd);
+  }
+  
+public:  
+  double operator()(NumericVector x,
+                                     NumericVector y) {
+    vecdub xn = as<vecdub>(x);
+    vecdub yn = as<vecdub>(y);
+    normalize(xn);
+    normalize(yn);
+    
+    double k = x.size() - 1;
+    k *= sd(xn);
+    k *= sd(yn);
+    
+    double mutualinfo = 0;
+    
+    for(pairit it(xn.begin(), yn.begin()) ; it.first != xn.end(); ++it.first, ++it.second) {
+      mutualinfo += *it.first *  *it.second;
+    }
+    
+    mutualinfo /= k;
+    return mutualinfo;
+  }
+};
