8312aef29f466e3c48f4248f42f21d55bb5fd6ba
Michael Galanakis <mgalanakis@sund.ku.dk>
2017-03-30 19:40:42 +0200

    Figuring out why my weights aren't uniform

---
+++ R/exploritory_plotting_weights.R
@@ -0,0 +1,35 @@
+# Weight plot
+library(ggplot2)
+
+Rcpp::sourceCpp("src/threshold_init.cpp")
+
+plot_weights <- function(w, probs = seq(0.1, .9, 0.1)) {
+  df <- data.frame(
+    obs = quantile(w, probs = probs),
+    teoretical = qchisq(probs, 1)
+  )
+  
+  ggplot(aes(x = teoretical, y = obs), data = df) +
+    geom_point() +
+    geom_abline(slope = 1, size = 1.5, col = "red") 
+}
+
+
+w <- attr(edges, "weights")
+w <- attr(cont_threshold_init(matrix(rnorm(1500), ncol = 150), 0), "weights")
+qplot(pchisq(w * 4, 1, log.p = FALSE), geom= "histogram")
+qplot(pchisq(w * 4, 1, lower.tail = FALSE, log.p = FALSE), geom= "histogram")
+
+- 0.5 * log(1 - cor(x, y)^2)
+attr(cont_threshold_init(matrix(c(x,y), ncol = 2), 0), "weights")
+     
+attr(cont_threshold_init(matrix(rnorm(50), ncol = 2), 0), "weights")
+qplot(pchisq(x  , 1, lower.tail = FALSE, log.p = FALSE))
+qplot(pchisq(x  , 1, lower.tail = TRUE, log.p = FALSE))
+
+x <- replicate(1000, { t.test(rnorm(100), rnorm(100))$p.value })
+qplot(x)
+w <- attr(edges, "weights")
+plot_weights(w)
+
+
