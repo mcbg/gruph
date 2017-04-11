d14d6297cec01b19a72d7e05b71a619002466d75
Michael Galanakis <mgalanakis@sund.ku.dk>
2017-04-11 14:08:44 +0200

    plotting weights now is constant as expected

--- R/exploritory_plotting_weights.R
+++ R/exploritory_plotting_weights.R
@@ -1,48 +1,18 @@
 # Weight plot
-library(ggplot2)
-
 Rcpp::sourceCpp("src/threshold_init.cpp")
 
-plot_weights <- function(w, probs = seq(0.1, .9, 0.1)) {
-  df <- data.frame(
-    obs = quantile(w, probs = probs),
-    teoretical = qchisq(probs, 1)
-  )
-  
-  ggplot(aes(x = teoretical, y = obs), data = df) +
-    geom_point() +
-    geom_abline(slope = 1, size = 1.5, col = "red") 
-}
+set.seed(123)
 
+N <- 100
+M <- 100
 
+df <- rnorm(n = N * M)
+dim(df) <- c(N, M)
+edges <- cont_threshold_init(df, 0)
 w <- attr(edges, "weights")
-w <- attr(cont_threshold_init(matrix(rnorm(1500), ncol = 150), 0), "weights")
-qplot(pchisq(w * 4, 1, log.p = FALSE), geom= "histogram")
-qplot(pchisq(w * 4, 1, lower.tail = FALSE, log.p = FALSE), geom= "histogram")
+ps <- pchisq(2 *  N * w, 1, lower.tail = TRUE)
+ggplot2::qplot(ps) # SICKENING!
 
-- 0.5 * log(1 - cor(x, y)^2)
-attr(cont_threshold_init(matrix(c(x,y), ncol = 2), 0), "weights")
-
-a <- replicate(1000, {
-  x <- rnorm(50)
-  y <- x + rnorm(50)
-  c(- 0.5 * log(1 - cor(x, y)^2),
-    cor(x,y), 
-  t.test(x,y)$statistic  )
-})
-
-qplot(a[2, ], a[3, ]) + xlim(0,1) + ylim(0,1)
-
-f <- function(x) pchisq(x, 1, lower.tail = FALSE)
-qplot(f(a))
-
-attr(cont_threshold_init(matrix(rnorm(50), ncol = 2), 0), "weights")
-qplot(pchisq(x  , 1, lower.tail = FALSE, log.p = FALSE))
-qplot(pchisq(x  , 1, lower.tail = TRUE, log.p = FALSE))
-
-x <- replicate(1000, { t.test(rnorm(100), rnorm(100))$p.value })
-qplot(x)
-w <- attr(edges, "weights")
-plot_weights(w)
-
-
+b <- (1:length(ps)) / length(ps)
+ggplot2::qplot(b, ps) +
+  geom_abline(slope = 0, intercept = 1, col = "red", size = 1.2)# SICKENING! 
