d10f5ccbc547de81c42f0abf7a1f74c8690f8068
Michael Galanakis <mgalanakis@sund.ku.dk>
2017-05-01 23:33:02 +0200

    moved plotting function to their own file

---
+++ R/plotting.R
@@ -0,0 +1,48 @@
+calc_pval <- function(edges, N) {
+  dfs <- attr(edges, "df")
+  ws <- attr(edges, "weights")
+  get_p_value <- function(w, degrees) pchisq(2 * N * w, degrees, lower.tail = TRUE)
+  unlist(Map(get_p_value, ws, dfs))
+} 
+
+pval_dataframe <- function(edges, N) {
+  pvals <- calc_pval(edges,N)
+  pvals <- pvals[order(-pvals)]
+  
+  data.frame(index = (1:length(pvals))/length(pvals),
+                   p = pvals)
+}
+
+weight_plot <- function(edges, N) {
+  # N: number of observations
+  
+  xx <- pval_dataframe(edges, N)
+  
+  ggplot(aes(x = index, y = p), data = xx) +
+    geom_point() 
+    #geom_abline(slope = -1, intercept = 1, col = "red", size = 1.2) 
+    #ylim(0, 1)
+    #facet_wrap(~df) #+ xlim(0, 1) + ylim(0, 1) 
+  # histogram:
+    #geom_histogram(binwidth = 0.1) + 
+    #geom_hline(0.1) +
+}
+
+weight_histogram <- function(edges, N, facet = TRUE){
+  # N: number of observations
+  
+  get_p_value <- function(w, degrees) pchisq(2 * N * w, degrees, lower.tail = TRUE)
+  w <- attr(edges, "weights")
+  dfs <- attr(edges, "df")
+  pvals <- unlist(Map(get_p_value, w, dfs))
+  pvals[order]
+  
+  xx <- data.frame(index = (1:length(pvals))/length(pvals),
+                   p = pvals)
+  
+  p <- ggplot(aes(x = p, col = df), data = xx) +
+    geom_histogram(binwidth = 0.1)
+  if (facet)
+    p <- p + facet_wrap(~df)
+  p
+}
