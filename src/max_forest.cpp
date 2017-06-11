8b331b1e93291456956dede8707f7cf1552c3112
Michael Galanakis <mgalanakis@sund.ku.dk>
2017-06-11 18:34:46 +0200

    max forest now works with new format, and uses penalized ML

--- src/max_forest.cpp
+++ src/max_forest.cpp
@@ -2,19 +2,31 @@
 // [[Rcpp::plugins(cpp11)]]
 
 #include <Rcpp.h>
-//#include "forest.h"
+#include <vector>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 
+#include "w_edge.h"
+#include "wrapper.h"
 
 using namespace Rcpp;
+using namespace std;
 using namespace boost;
 
 struct inv_double
 {
-    //just inverts `operator<` so to find maximum forest
-    double x;
-    int df;
+  inv_double(double pw,
+             int pdf,
+             double penalty)
+  : w(pw),
+    df(pdf),
+    x(pw - pdf * penalty) {}
+  
+  inv_double() : inv_double(0, 0, 0) {} 
+  
+  double w;
+  int df;
+  double x;
 };
 
 bool operator>(inv_double x, inv_double y) {
@@ -37,61 +49,57 @@ bool operator>=(inv_double x, inv_double y)
 }
 
 // [[Rcpp::export]]
-NumericVector max_forest(NumericMatrix x) {
+DataFrame max_forest(DataFrame x, int num_nodes, double penalty)
+{
+  typedef vector<inv_double> Weights; 
+  typedef pair<int, int> E; 
+  typedef vector<E> Edges; 
   
-  typedef std::vector<inv_double> Weights;
-  typedef std::pair<int, int> E;
-  typedef std::vector<E> edges;
-  
-  size_t num_nodes = x.ncol();
-  NumericVector w = x.attr("weights");
-  NumericVector df = x.attr("df");
-  
-  // CONVERT TO CPP OBJECTS
-  edges mEdges(x.nrow(), {-1, -1});
-  Weights weights(w.size()); // = as<Weights>(w);
-  
-  for(int i = 0; i < x.nrow(); ++i) {
-    mEdges[i].first = x(i,0);
-    mEdges[i].second = x(i,1);
-    weights[i].x = w(i);
-    weights[i].df = df(i);
-  }
-  
-  
-  // make boost graph
   typedef property<edge_weight_t, inv_double> EdgeWeightProperty;
   typedef adjacency_list<vecS, vecS, undirectedS, no_property, EdgeWeightProperty> graph;
-  
-  graph g(mEdges.begin(), mEdges.end(), weights.begin(), num_nodes);
- 
   typedef graph_traits < graph >::edge_descriptor Edge;
+  
+  NumericVector first = x["i"];
+  NumericVector second = x["j"];
+  NumericVector w = x["weights"];
+  NumericVector df = x["df"];
+  
+  Edges edges_(x.nrow(), {-1, -1});
+  Weights weights_(w.size());
+  
+  for(int i = 0; i < x.nrow(); ++i) {
+    edges_[i].first = first[i];
+    edges_[i].second = second[i];
+    
+    weights_[i] = inv_double(w[i], df[i], penalty);
+  }
+  
+  // MAKE BOOST GRAPH
+  graph g(edges_.begin(), edges_.end(), weights_.begin(), num_nodes);
+ 
   std::vector< Edge > spanning_forest;
   kruskal_minimum_spanning_tree(g, std::back_inserter(spanning_forest));
   
   // export
-  NumericMatrix out(spanning_forest.size(), 2);
-  NumericVector out_w(spanning_forest.size());
-  NumericVector out_df(spanning_forest.size());
+  const size_t k = spanning_forest.size();
+  NumericVector out_first(k);
+  NumericVector out_second(k);
+  NumericVector out_w(k);
+  NumericVector out_df(k);
   
   int i = 0;
   for (std::vector < Edge >::iterator ei = spanning_forest.begin();
        ei != spanning_forest.end();
        ++ei, ++i) {
-    out(i, 0) = source(*ei, g);
-    out(i, 1) = target(*ei, g);
-    out_w(i) = get(edge_weight, g, *ei).x;
+    out_first(i) = source(*ei, g);
+    out_second(i) = target(*ei, g);
+    out_w(i) = get(edge_weight, g, *ei).w;
     out_df(i) = get(edge_weight, g, *ei).df;
   }
-  out.attr("weights") = out_w;
-  out.attr("df") = out_df;
-  out.attr("columns") = x.attr("columns");
   
-  return out;
+  return DataFrame::create(_("i") = out_first,
+                   _("j") = out_second,
+                   _("weights") = out_w,
+                   _("df") = out_df);
 }
 
-// [[Rcpp::export]]
-NumericVector max_forest_c(CharacterMatrix x) {
-  NumericMatrix y = as<NumericMatrix>(x);
-  return max_forest(y);
-}
